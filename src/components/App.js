4b2f7e3035e7a83f14625fe0a534d7e52a54de49
Samantha <samanthavillafana8@gmail.com>
2024-05-08 20:12:33 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [fourSushis, setFourSushis] = useState([])
+  const [budget, setBudget] = useState(100)
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      const allSushis = data.map(s => {
+        return {...s, eaten: false}
+      })
+      const four = allSushis.splice(0,4)
+      setSushis(allSushis)
+      setFourSushis(four)
+    })
+  }, [])
+
+  const getSushi = () => {
+    const allSushis = sushis
+    const four = allSushis.splice(0,4)
+    setFourSushis(four)
+  }
+
+  const eatSushi = (e) => {
+    if (budget - parseInt(e.target.dataset.price) >= 0) {
+      const newSushis = fourSushis.map(s => s.id == e.target.id ? {...s, eaten: true } : s)
+      setFourSushis(newSushis)
+      const newBudget = budget - parseInt(e.target.dataset.price)
+      setBudget(newBudget)
+    }
+  }
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer fourSushis = {fourSushis} eatSushi={eatSushi} getSushi={getSushi}/>
+      <Table fourSushis = {fourSushis} budget={budget}/>
     </div>
   );
 }
